81d21a85a09ba157f135e87cab0208b1a976342c
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-08-16 18:08:03 +0530

    feat(aks-nodepool): add nodepool example

--- azurem/modules/nodepool/versions.tf
+++ azurem/modules/nodepool/versions.tf
@@ -8,15 +8,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = ">= 2.10.0"
     }
   }
 }
