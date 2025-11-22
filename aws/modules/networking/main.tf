ab92a0fa47cfe80dc42b00cda4032557f30ba1a0
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-11-22 20:44:46 +0530

    add ecr endpoints

--- aws/modules/networking/main.tf
+++ aws/modules/networking/main.tf
@@ -43,26 +43,34 @@ module "vpc_endpoints" {
   security_group_ids = [aws_security_group.vpc_endpoints[0].id]
 
   endpoints = {
+    # we store metadata in s3 all pod requests go through this endpoint
     s3 = {
       service         = "s3"
       service_type    = "Gateway"
       route_table_ids = module.vpc.private_route_table_ids
       tags            = { Name = "${var.name_prefix}-s3-gateway-endpoint" }
     }
+
     # not adding aws rds endpoint as it will add up unnecessary cost
     # pods connect to rds using private ip. The traffic doesn't leave vpc so not needed.
-    # we would endup paying extra $0.02*24*30 = $14 per month even if we don't use it.
+    # we would endup paying extra $0.02*24*30 = $14.4 per month even if we don't use it.
     # https://aws.amazon.com/privatelink/pricing/
+
+    # ec2 api is useful for Karpenter
     ec2 = {
       service             = "ec2"
       private_dns_enabled = true
       tags                = { Name = "${var.name_prefix}-ec2-endpoint" }
     }
+
+    # not needed secretsmanager endpoint as we use k8s secrets, discuss and remove it.
     secretsmanager = {
       service             = "secretsmanager"
       private_dns_enabled = true
       tags                = { Name = "${var.name_prefix}-secretsmanager-endpoint" }
     }
+
+    # Required for AWS Session Manager to work, useful to ssh into worker nodes via aws console/cli
     ssm = {
       service             = "ssm"
       private_dns_enabled = true
@@ -78,20 +86,38 @@ module "vpc_endpoints" {
       private_dns_enabled = true
       tags                = { Name = "${var.name_prefix}-ec2messages-endpoint" }
     }
+
+    # sts endpoint is useful for IRSA
     sts = {
       service             = "sts"
       private_dns_enabled = true
       tags                = { Name = "${var.name_prefix}-sts-endpoint" }
     }
+
+    # not needed kms endpoint as we rely on ebs encryption in nodes, discuss and remove it.
     kms = {
       service             = "kms"
       private_dns_enabled = true
       tags                = { Name = "${var.name_prefix}-kms-endpoint" }
     }
+
+    # Allows private access to the ELB API to create/manage load balancers. Useful for aws-lbc
     elasticloadbalancing = {
       service             = "elasticloadbalancing"
       private_dns_enabled = true
       tags                = { Name = "${var.name_prefix}-elasticloadbalancing-endpoint" }
+    }
+
+    # for image pulls from ecr
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      tags                = { Name = "${var.name_prefix}-ecr-api-endpoint" }
+    }
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      tags                = { Name = "${var.name_prefix}-ecr-dkr-endpoint" }
     }
   }
 
