b8493f5cdf9b4c2590b9da4caf16af1978b0e9d0
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-12-10 23:03:19 +0530

    quotes

--- aws/modules/karpenter-nodepool/main.tf
+++ aws/modules/karpenter-nodepool/main.tf
@@ -22,7 +22,7 @@ resource "terraform_data" "destroyer" {
       fi
 
       kubeconfig_file=$(mktemp)
-      trap "rm -f "$${kubeconfig_file}" EXIT
+      trap "rm -f '$${kubeconfig_file}' EXIT
       echo "$${KUBECONFIG_DATA}" > "$${kubeconfig_file}"
 
       nodeclaims=$(kubectl --kubeconfig "$${kubeconfig_file}" get nodeclaims -l "karpenter.sh/nodepool=$${NODEPOOL_NAME}" -o name)
