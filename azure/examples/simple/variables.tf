c8a0505cba10df1dc14fba1b9e17c33d9d661152
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-12-12 17:41:00 +0530

    add readme steps to configure authorized ip ranges

--- azure/examples/simple/variables.tf
+++ azure/examples/simple/variables.tf
@@ -30,3 +30,21 @@ variable "license_key" {
   default     = null
   sensitive   = true
 }
+
+variable "api_server_authorized_ip_ranges" {
+  description = "List of authorized IP ranges that can access the Kubernetes API server when public access is available. Defaults to ['0.0.0.0/0'] (allow all). For production, restrict to specific IPs (e.g., ['203.0.113.0/24'])"
+  type        = list(string)
+  default     = ["0.0.0.0/0"] # Explicit default: allow all IPs
+  nullable    = true
+
+  validation {
+    condition = (
+      var.api_server_authorized_ip_ranges == null ||
+      alltrue([
+        for cidr in var.api_server_authorized_ip_ranges :
+        can(cidrhost(cidr, 0))
+      ])
+    )
+    error_message = "All api_server_authorized_ip_ranges must be valid CIDR blocks (e.g., '203.0.113.0/24')."
+  }
+}
