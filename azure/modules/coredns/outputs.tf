304c015fe279fd91b26a9ce05ca272ce0c553d9f
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-12-23 23:58:22 +0530

    azure uses common coredns

--- azure/modules/coredns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "deployment_name" {
-  description = "Name of the custom CoreDNS deployment"
-  value       = kubernetes_deployment.coredns.metadata[0].name
-}
-
-output "config_map_name" {
-  description = "Name of the CoreDNS ConfigMap"
-  value       = kubernetes_config_map.coredns.metadata[0].name
-}
