efb34dc5e42f3047aeca9466f754eed4ec9277b3
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2026-01-06 17:45:17 +0530

    create role

--- kubernetes/modules/materialize-instance/outputs.tf
+++ kubernetes/modules/materialize-instance/outputs.tf
@@ -22,3 +22,21 @@ output "persist_backend_url" {
   description = "Persist backend URL used by the Materialize instance"
   value       = var.persist_backend_url
 }
+
+output "mz_system_credentials" {
+  description = "Credentials for the mz_system user, not to be used by applications"
+  value = contains(["Password", "Sasl"], var.authenticator_kind) ? {
+    username = "mz_system"
+    password = var.external_login_password_mz_system
+  } : {}
+  sensitive = true
+}
+
+output "service_account_credentials" {
+  description = "Credentials for the default service account with superuser privileges, Login to materialize using these credentials"
+  value = contains(["Password", "Sasl"], var.authenticator_kind) ? {
+    username = local.service_account_name
+    password = random_password.service_account_password[0].result
+  } : {}
+  sensitive = true
+}
