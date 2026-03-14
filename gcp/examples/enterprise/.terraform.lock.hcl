55f3c137bf24f21632a70ac4ced5244cd6094f56
bobbyiliev <bobby@bobbyiliev.com>
2026-03-14 19:56:14 +0200

    Add GCP enterprise example with Ory Kratos and Hydra for OIDC/SAML authentication

---
+++ gcp/examples/enterprise/.terraform.lock.hcl
@@ -0,0 +1,160 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/alekc/kubectl" {
+  version     = "2.1.5"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:ZMqwfwUL49Nvp5LZims1nt7zdE2Lj9rtVHkRobYWBCs=",
+    "zh:11d6c7e429d013ce4b224a8e778fa1e272743dd131696039c65f8238d0c5afee",
+    "zh:39e55dc62ee6c31032e0ccd4f4d87d28f62ecf2025b726b705327b91235bb5d7",
+    "zh:3ef4cc118f1e9586bf794dcc9d0ed7912815df182239c28111fa2d67d4d9f264",
+    "zh:4183684cbd5a12c5b716b5d349178ac7cedda277ed0bc5cca2a03e33c0c6ff2d",
+    "zh:79e2d64ad8035a4254d40bc0f5064c3bb3a3d38e4e6d969c810f6512a50106f7",
+    "zh:91961239727bd01fd145752d5f42877bf8b8087a095ab68c39e3068941cdc09c",
+    "zh:a94b91e527d68a85aa5ab020c27d383bfa4aa61bf585248f3591808c8e29841d",
+    "zh:b0cdb266cc46d58ef3558f86e94d3d3718c6bb1281f2357c50bd7d2b6828e11a",
+    "zh:c6b12ca20041ed7b3cb86ff15cf69f8c6c4ccfe94640b8f45386ebcf26adfc31",
+    "zh:d7424e9d3511af81174549716c71da175529e439525fa44746b69ca514ab9022",
+    "zh:decb856ecf18a5a51775953233d3203ad2468456a6a28a4ff5243ed2d9a3241e",
+    "zh:e2b176e5cd860ea2559965d7bab34ccb6f2edcc6c8b92c1d96853ac1368f0415",
+    "zh:f780ee23ce21fb674d57f60320a284cdaf40d3baba4e3f8a274ad4601c989317",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "6.50.0"
+  constraints = ">= 3.33.0, >= 3.83.0, >= 4.25.0, >= 4.51.0, >= 4.64.0, >= 6.19.0, >= 6.31.0, < 7.0.0"
+  hashes = [
+    "h1:79CwMTsp3Ud1nOl5hFS5mxQHyT0fGVye7pqpU0PPlHI=",
+    "zh:1f3513fcfcbf7ca53d667a168c5067a4dd91a4d4cccd19743e248ff31065503c",
+    "zh:3da7db8fc2c51a77dd958ea8baaa05c29cd7f829bd8941c26e2ea9cb3aadc1e5",
+    "zh:3e09ac3f6ca8111cbb659d38c251771829f4347ab159a12db195e211c76068bb",
+    "zh:7bb9e41c568df15ccf1a8946037355eefb4dfb4e35e3b190808bb7c4abae547d",
+    "zh:81e5d78bdec7778e6d67b5c3544777505db40a826b6eb5abe9b86d4ba396866b",
+    "zh:8d309d020fb321525883f5c4ea864df3d5942b6087f6656d6d8b3a1377f340fc",
+    "zh:93e112559655ab95a523193158f4a4ac0f2bfed7eeaa712010b85ebb551d5071",
+    "zh:d3efe589ffd625b300cef5917c4629513f77e3a7b111c9df65075f76a46a63c7",
+    "zh:d4a4d672bbef756a870d8f32b35925f8ce2ef4f6bbd5b71a3cb764f1b6c85421",
+    "zh:e13a86bca299ba8a118e80d5f84fbdd708fe600ecdceea1a13d4919c068379fe",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:fec30c095647b583a246c39d557704947195a1b7d41f81e369ba377d997faef6",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version     = "6.50.0"
+  constraints = ">= 4.64.0, >= 6.19.0, >= 6.31.0, < 7.0.0"
+  hashes = [
+    "h1:P2GiUJM1frlPtBViwKn1A9V2dVBdGuWcX80w9TdH8ZE=",
+    "zh:18b442bd0a05321d39dda1e9e3f1bdede4e61bc2ac62cc7a67037a3864f75101",
+    "zh:2e387c51455862828bec923a3ec81abf63a4d998da470cf00e09003bda53d668",
+    "zh:3942e708fa84ebe54996086f4b1398cb747fe19cbcd0be07ace528291fb35dee",
+    "zh:496287dd48b34ae6197cb1f887abeafd07c33f389dbe431bb01e24846754cfdd",
+    "zh:6eca885419969ce5c2a706f34dce1f10bde9774757675f2d8a92d12e5a1be390",
+    "zh:710dbef826c3fe7f76f844dae47937e8e4c1279dd9205ec4610be04cf3327244",
+    "zh:777ebf44b24bfc7bdbf770dc089f1a72f143b4718fdedb8c6bd75983115a1ec2",
+    "zh:9c8703bba37b8c7ad857efc3513392c5a096c519397c1cb822d7612f38e4262f",
+    "zh:c4f1d3a73de2702277c99d5348ad6d374705bcfdd367ad964ff4cfd2cf06c281",
+    "zh:eca8df11af3f5a948492d5b8b5d01b4ec705aad10bc30ec1524205508ae28393",
+    "zh:f41e7fd5f2628e8fd6b8ea136366923858f54428d1729898925469b862c275c2",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/helm" {
+  version     = "2.17.0"
+  constraints = "~> 2.0, >= 2.5.0"
+  hashes = [
+    "h1:kQMkcPVvHOguOqnxoEU2sm1ND9vCHiT8TvZ2x6v/Rsw=",
+    "zh:06fb4e9932f0afc1904d2279e6e99353c2ddac0d765305ce90519af410706bd4",
+    "zh:104eccfc781fc868da3c7fec4385ad14ed183eb985c96331a1a937ac79c2d1a7",
+    "zh:129345c82359837bb3f0070ce4891ec232697052f7d5ccf61d43d818912cf5f3",
+    "zh:3956187ec239f4045975b35e8c30741f701aa494c386aaa04ebabffe7749f81c",
+    "zh:66a9686d92a6b3ec43de3ca3fde60ef3d89fb76259ed3313ca4eb9bb8c13b7dd",
+    "zh:88644260090aa621e7e8083585c468c8dd5e09a3c01a432fb05da5c4623af940",
+    "zh:a248f650d174a883b32c5b94f9e725f4057e623b00f171936dcdcc840fad0b3e",
+    "zh:aa498c1f1ab93be5c8fbf6d48af51dc6ef0f10b2ea88d67bcb9f02d1d80d3930",
+    "zh:bf01e0f2ec2468c53596e027d376532a2d30feb72b0b5b810334d043109ae32f",
+    "zh:c46fa84cc8388e5ca87eb575a534ebcf68819c5a5724142998b487cb11246654",
+    "zh:d0c0f15ffc115c0965cbfe5c81f18c2e114113e7a1e6829f6bfd879ce5744fbb",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.38.0"
+  constraints = "~> 2.0, >= 2.10.0"
+  hashes = [
+    "h1:soK8Lt0SZ6dB+HsypFRDzuX/npqlMU6M0fvyaR1yW0k=",
+    "zh:0af928d776eb269b192dc0ea0f8a3f0f5ec117224cd644bdacdc682300f84ba0",
+    "zh:1be998e67206f7cfc4ffe77c01a09ac91ce725de0abaec9030b22c0a832af44f",
+    "zh:326803fe5946023687d603f6f1bab24de7af3d426b01d20e51d4e6fbe4e7ec1b",
+    "zh:4a99ec8d91193af961de1abb1f824be73df07489301d62e6141a656b3ebfff12",
+    "zh:5136e51765d6a0b9e4dbcc3b38821e9736bd2136cf15e9aac11668f22db117d2",
+    "zh:63fab47349852d7802fb032e4f2b6a101ee1ce34b62557a9ad0f0f0f5b6ecfdc",
+    "zh:924fb0257e2d03e03e2bfe9c7b99aa73c195b1f19412ca09960001bee3c50d15",
+    "zh:b63a0be5e233f8f6727c56bed3b61eb9456ca7a8bb29539fba0837f1badf1396",
+    "zh:d39861aa21077f1bc899bc53e7233262e530ba8a3a2d737449b100daeb303e4d",
+    "zh:de0805e10ebe4c83ce3b728a67f6b0f9d18be32b25146aa89116634df5145ad4",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:faf23e45f0090eef8ba28a8aac7ec5d4fdf11a36c40a8d286304567d71c1e7db",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version     = "3.2.4"
+  constraints = "~> 3.0, ~> 3.1"
+  hashes = [
+    "h1:L5V05xwp/Gto1leRryuesxjMfgZwjb7oool4WS1UEFQ=",
+    "zh:59f6b52ab4ff35739647f9509ee6d93d7c032985d9f8c6237d1f8a59471bbbe2",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:795c897119ff082133150121d39ff26cb5f89a730a2c8c26f3a9c1abf81a9c43",
+    "zh:7b9c7b16f118fbc2b05a983817b8ce2f86df125857966ad356353baf4bff5c0a",
+    "zh:85e33ab43e0e1726e5f97a874b8e24820b6565ff8076523cc2922ba671492991",
+    "zh:9d32ac3619cfc93eb3c4f423492a8e0f79db05fec58e449dee9b2d5873d5f69f",
+    "zh:9e15c3c9dd8e0d1e3731841d44c34571b6c97f5b95e8296a45318b94e5287a6e",
+    "zh:b4c2ab35d1b7696c30b64bf2c0f3a62329107bd1a9121ce70683dec58af19615",
+    "zh:c43723e8cc65bcdf5e0c92581dcbbdcbdcf18b8d2037406a5f2033b1e22de442",
+    "zh:ceb5495d9c31bfb299d246ab333f08c7fb0d67a4f82681fbf47f2a21c3e11ab5",
+    "zh:e171026b3659305c558d9804062762d168f50ba02b88b231d20ec99578a6233f",
+    "zh:ed0fe2acdb61330b01841fa790be00ec6beaac91d41f311fb8254f74eb6a711f",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.8.1"
+  constraints = ">= 3.0.0, ~> 3.0, ~> 3.1"
+  hashes = [
+    "h1:u8AKlWVDTH5r9YLSeswoVEjiY72Rt4/ch7U+61ZDkiQ=",
+    "zh:08dd03b918c7b55713026037c5400c48af5b9f468f483463321bd18e17b907b4",
+    "zh:0eee654a5542dc1d41920bbf2419032d6f0d5625b03bd81339e5b33394a3e0ae",
+    "zh:229665ddf060aa0ed315597908483eee5b818a17d09b6417a0f52fd9405c4f57",
+    "zh:2469d2e48f28076254a2a3fc327f184914566d9e40c5780b8d96ebf7205f8bc0",
+    "zh:37d7eb334d9561f335e748280f5535a384a88675af9a9eac439d4cfd663bcb66",
+    "zh:741101426a2f2c52dee37122f0f4a2f2d6af6d852cb1db634480a86398fa3511",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:a902473f08ef8df62cfe6116bd6c157070a93f66622384300de235a533e9d4a9",
+    "zh:b85c511a23e57a2147355932b3b6dce2a11e856b941165793a0c3d7578d94d05",
+    "zh:c5172226d18eaac95b1daac80172287b69d4ce32750c82ad77fa0768be4ea4b8",
+    "zh:dab4434dba34aad569b0bc243c2d3f3ff86dd7740def373f2a49816bd2ff819b",
+    "zh:f49fd62aa8c5525a5c17abd51e27ca5e213881d58882fd42fec4a545b53c9699",
+  ]
+}
+
+provider "registry.terraform.io/isometry/deepmerge" {
+  version     = "1.2.1"
+  constraints = "~> 1.0"
+  hashes = [
+    "h1:+GBRWyzNYKj47qmSBcV28lrIIgk3Gusj48maI+jrL0Q=",
+    "zh:13be4c31971addc10e26a003e22b8867dba41737ffbc9de86ed84555c4a539b7",
+    "zh:320a939a594c8a2563f4c11108e02428e7fda3bd51a4fc2298089299cf23f516",
+    "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
+    "zh:9407fe1f9d332ecbd5252faa5b04f62b7fa47f19efe35a9e92d30ef2c603db45",
+    "zh:a0af6ad4d4a52a1355df5b73fe38e406f50d9fd5c8af165929a602f906de0ff7",
+    "zh:a44628f6e9bc612dbdcb013a8cf33aca3893f1697c4492847859f7db5b59c4dd",
+    "zh:abaaa7d71f7975505824adcebed320aae43fc7e13c901ffc51448f6eb59585fe",
+    "zh:ad2b1b3f348ad478ee64d294a85835625e560324e58a826530b13f49fafa9bac",
+    "zh:e8bec252634868283e47ef1208cb89b53c1e0cdd2ad804acf91d0ff368048416",
+  ]
+}
---
+++ gcp/examples/enterprise/main.tf
@@ -0,0 +1,566 @@
+provider "google" {
+  project        = var.project_id
+  region         = var.region
+  default_labels = var.labels
+}
+
+# Configure kubernetes provider with GKE cluster credentials
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.cluster_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+  }
+}
+
+provider "kubectl" {
+  host                   = "https://${module.gke.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+
+  load_config_file = false
+}
+
+locals {
+
+  materialize_operator_namespace = "materialize"
+  materialize_instance_namespace = "materialize-environment"
+  materialize_instance_name      = "main"
+
+  # Common node scheduling configuration
+  generic_node_labels = {
+    "workload" = "generic"
+  }
+
+  materialize_node_labels = {
+    "workload" = "materialize-instance"
+  }
+
+  materialize_node_taints = [
+    {
+      key    = "materialize.cloud/workload"
+      value  = "materialize-instance"
+      effect = "NO_SCHEDULE"
+    }
+  ]
+
+  materialize_tolerations = [
+    {
+      key      = "materialize.cloud/workload"
+      value    = "materialize-instance"
+      operator = "Equal"
+      effect   = "NoSchedule"
+    }
+  ]
+
+
+  subnets = [
+    {
+      name           = "${var.name_prefix}-subnet"
+      cidr           = "192.168.0.0/20"
+      region         = var.region
+      private_access = true
+      secondary_ranges = [
+        {
+          range_name    = "pods"
+          ip_cidr_range = "192.168.64.0/18"
+        },
+        {
+          range_name    = "services"
+          ip_cidr_range = "192.168.128.0/20"
+        }
+      ]
+    }
+  ]
+
+  gke_config = {
+    machine_type = "n2-highmem-8"
+    disk_size_gb = 100
+    min_nodes    = 2
+    max_nodes    = 5
+  }
+
+  database_config = {
+    tier      = "db-custom-2-4096"
+    database  = { name = "materialize", charset = "UTF8", collation = "en_US.UTF8" }
+    user_name = "materialize"
+  }
+
+  # Ory database configuration (separate Cloud SQL instance)
+  ory_database_config = {
+    tier      = "db-f1-micro"
+    user_name = "oryadmin"
+  }
+
+  local_ssd_count = 1
+  swap_enabled    = true
+
+  database_statement_timeout = "15min"
+
+  metadata_backend_url = format(
+    "postgres://%s:%s@%s/%s?sslmode=require&options=-c%%20statement_timeout%%3D%s",
+    module.database.users[0].name,
+    urlencode(module.database.users[0].password),
+    module.database.private_ip,
+    local.database_config.database.name,
+    local.database_statement_timeout
+  )
+
+
+  encoded_endpoint = urlencode("https://storage.googleapis.com")
+  encoded_secret   = urlencode(module.storage.hmac_secret)
+
+  persist_backend_url = format(
+    "s3://%s:%s@%s/materialize?endpoint=%s&region=%s",
+    module.storage.hmac_access_id,
+    local.encoded_secret,
+    module.storage.bucket_name,
+    local.encoded_endpoint,
+    var.region
+  )
+
+  kubeconfig_data = jsonencode({
+    apiVersion = "v1"
+    kind       = "Config"
+    clusters = [{
+      name = module.gke.cluster_name
+      cluster = {
+        certificate-authority-data = module.gke.cluster_ca_certificate
+        server                     = "https://${module.gke.cluster_endpoint}"
+      }
+    }]
+    contexts = [{
+      name = module.gke.cluster_name
+      context = {
+        cluster = module.gke.cluster_name
+        user    = module.gke.cluster_name
+      }
+    }]
+    current-context = module.gke.cluster_name
+    users = [{
+      name = module.gke.cluster_name
+      user = {
+        token : data.google_client_config.default.access_token
+      }
+    }]
+  })
+  storage_class = "standard-rwo" # default storage class in gcp
+
+  # Ory database DSNs
+  ory_kratos_dsn = format(
+    "postgres://%s:%s@%s/%s?sslmode=disable",
+    module.ory_database.users[0].name,
+    urlencode(module.ory_database.users[0].password),
+    module.ory_database.private_ip,
+    "kratos"
+  )
+
+  ory_hydra_dsn = format(
+    "postgres://%s:%s@%s/%s?sslmode=disable",
+    module.ory_database.users[0].name,
+    urlencode(module.ory_database.users[0].password),
+    module.ory_database.private_ip,
+    "hydra"
+  )
+}
+
+# Configure networking infrastructure including VPC, subnets, and CIDR blocks
+module "networking" {
+  source = "../../modules/networking"
+
+  project_id = var.project_id
+  region     = var.region
+  prefix     = var.name_prefix
+  subnets    = local.subnets
+  labels     = var.labels
+}
+
+# Set up Google Kubernetes Engine (GKE) cluster
+module "gke" {
+  source = "../../modules/gke"
+
+  depends_on = [module.networking]
+
+  project_id   = var.project_id
+  region       = var.region
+  prefix       = var.name_prefix
+  network_name = module.networking.network_name
+  # we only have one subnet, so we can use the first one
+  # if multiple subnets are created, we need to use the specific subnet name here
+  subnet_name                       = module.networking.subnets_names[0]
+  namespace                         = local.materialize_operator_namespace
+  k8s_apiserver_authorized_networks = var.k8s_apiserver_authorized_networks
+  labels                            = var.labels
+}
+
+# Create and configure generic node pool for all workloads except Materialize
+module "generic_nodepool" {
+  source     = "../../modules/nodepool"
+  depends_on = [module.gke]
+
+  prefix                = "${var.name_prefix}-generic"
+  region                = var.region
+  enable_private_nodes  = true
+  cluster_name          = module.gke.cluster_name
+  project_id            = var.project_id
+  min_nodes             = 2
+  max_nodes             = 5
+  machine_type          = "e2-standard-8"
+  disk_size_gb          = 50
+  service_account_email = module.gke.service_account_email
+  labels                = local.generic_node_labels
+  swap_enabled          = false
+  local_ssd_count       = 0
+}
+
+# Create and configure Materialize-dedicated node pool with taints
+module "materialize_nodepool" {
+  source     = "../../modules/nodepool"
+  depends_on = [module.gke]
+
+  prefix                = "${var.name_prefix}-mz"
+  region                = var.region
+  enable_private_nodes  = true
+  cluster_name          = module.gke.cluster_name
+  project_id            = var.project_id
+  min_nodes             = local.gke_config.min_nodes
+  max_nodes             = local.gke_config.max_nodes
+  machine_type          = local.gke_config.machine_type
+  disk_size_gb          = local.gke_config.disk_size_gb
+  service_account_email = module.gke.service_account_email
+  labels                = merge(var.labels, local.materialize_node_labels)
+  # Materialize-specific taint to isolate workloads
+  node_taints = local.materialize_node_taints
+
+  swap_enabled    = local.swap_enabled
+  local_ssd_count = local.local_ssd_count
+}
+
+# Deploy custom CoreDNS with TTL 0 (GKE's kube-dns doesn't support disabling caching)
+module "coredns" {
+  source                                      = "../../../kubernetes/modules/coredns"
+  create_coredns_service_account              = true
+  node_selector                               = local.generic_node_labels
+  kubeconfig_data                             = local.kubeconfig_data
+  coredns_deployment_to_scale_down            = "kube-dns"
+  coredns_autoscaler_deployment_to_scale_down = "kube-dns-autoscaler"
+  depends_on = [
+    module.gke,
+    module.generic_nodepool,
+  ]
+}
+
+resource "random_password" "external_login_password_mz_system" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# Set up PostgreSQL database instance for Materialize metadata storage
+module "database" {
+  source     = "../../modules/database"
+  depends_on = [module.networking]
+
+  databases = [local.database_config.database]
+  # We don't provide password, so random password is generated
+  users = [{ name = local.database_config.user_name }]
+
+  project_id = var.project_id
+  region     = var.region
+  prefix     = var.name_prefix
+  network_id = module.networking.network_id
+
+  tier = local.database_config.tier
+
+  labels = var.labels
+}
+
+# Separate Cloud SQL instance for Ory (Kratos + Hydra)
+module "ory_database" {
+  source     = "../../modules/database"
+  depends_on = [module.networking]
+
+  databases = [
+    { name = "kratos", charset = "UTF8", collation = "en_US.UTF8" },
+    { name = "hydra", charset = "UTF8", collation = "en_US.UTF8" }
+  ]
+  users = [{ name = local.ory_database_config.user_name }]
+
+  project_id = var.project_id
+  region     = var.region
+  prefix     = "${var.name_prefix}-ory"
+  network_id = module.networking.network_id
+
+  tier = local.ory_database_config.tier
+
+  labels = var.labels
+}
+
+# Create Google Cloud Storage bucket for Materialize persistent data storage
+module "storage" {
+  source = "../../modules/storage"
+
+  project_id      = var.project_id
+  region          = var.region
+  prefix          = var.name_prefix
+  service_account = module.gke.workload_identity_sa_email
+  versioning      = false
+  version_ttl     = 7
+
+  labels = var.labels
+}
+
+# Install cert-manager for SSL certificate management and create cluster issuer
+module "cert_manager" {
+  source = "../../../kubernetes/modules/cert-manager"
+
+  node_selector = local.generic_node_labels
+
+  depends_on = [
+    module.gke,
+    module.generic_nodepool,
+    module.coredns,
+  ]
+}
+
+module "self_signed_cluster_issuer" {
+  source = "../../../kubernetes/modules/self-signed-cluster-issuer"
+
+  name_prefix = var.name_prefix
+
+  depends_on = [
+    module.cert_manager,
+  ]
+}
+
+# Install Materialize Kubernetes operator for managing Materialize instances
+module "operator" {
+  source = "../../modules/operator"
+
+  name_prefix = var.name_prefix
+  region      = var.region
+
+  # ARM tolerations and node selector for all operator workloads on GCP
+  instance_pod_tolerations = local.materialize_tolerations
+  instance_node_selector   = local.materialize_node_labels
+
+  # node selector for operator and metrics-server workloads
+  operator_node_selector = local.generic_node_labels
+
+
+  # Enable Prometheus scrape annotations when observability is enabled
+  helm_values = var.enable_observability ? {
+    observability = {
+      enabled = true
+      prometheus = {
+        scrapeAnnotations = {
+          enabled = true
+        }
+      }
+    }
+  } : {}
+  enable_network_policies = true
+  depends_on = [
+    module.gke,
+    module.generic_nodepool,
+    module.database,
+    module.storage,
+    module.coredns,
+  ]
+}
+
+module "prometheus" {
+  count  = var.enable_observability ? 1 : 0
+  source = "../../../kubernetes/modules/prometheus"
+
+  namespace        = "monitoring"
+  create_namespace = false # operator creates the "monitoring" namespace
+  node_selector    = local.generic_node_labels
+  storage_class    = local.storage_class
+  depends_on = [
+    module.operator,
+    module.gke,
+    module.generic_nodepool,
+    module.coredns,
+  ]
+}
+
+module "grafana" {
+  count  = var.enable_observability ? 1 : 0
+  source = "../../../kubernetes/modules/grafana"
+
+  namespace     = "monitoring"
+  storage_class = local.storage_class
+  # operator creates the "monitoring" namespace
+  create_namespace = false
+  prometheus_url   = module.prometheus[0].prometheus_url
+  node_selector    = local.generic_node_labels
+
+  depends_on = [
+    module.prometheus,
+  ]
+}
+
+# Deploy Materialize instance with configured backend connections
+module "materialize_instance" {
+  source                  = "../../../kubernetes/modules/materialize-instance"
+  instance_name           = local.materialize_instance_name
+  instance_namespace      = local.materialize_instance_namespace
+  metadata_backend_url    = local.metadata_backend_url
+  persist_backend_url     = local.persist_backend_url
+  enable_network_policies = true
+
+  # The password for the external login to the Materialize instance
+  external_login_password_mz_system = random_password.external_login_password_mz_system.result
+  authenticator_kind                = "Password"
+
+  # GCP workload identity annotation for service account
+  # TODO: this needs a fix in Environmentd Client. KSA based access to storage doesn't work end to end
+  service_account_annotations = {
+    "iam.gke.io/gcp-service-account" = module.gke.workload_identity_sa_email
+  }
+
+  license_key = var.license_key
+
+  issuer_ref = {
+    name = module.self_signed_cluster_issuer.issuer_name
+    kind = "ClusterIssuer"
+  }
+
+  # System parameters for the Materialize instance
+  # See: https://materialize.com/docs/self-managed-deployments/configuration-system-parameters/
+  system_parameters = {}
+
+  depends_on = [
+    module.gke,
+    module.database,
+    module.storage,
+    module.networking,
+    module.self_signed_cluster_issuer,
+    module.operator,
+    module.materialize_nodepool,
+    module.coredns,
+  ]
+}
+
+# Configure load balancers for external access to Materialize services
+module "load_balancers" {
+  source = "../../modules/load_balancers"
+
+  project_id                 = var.project_id
+  network_name               = module.networking.network_name
+  prefix                     = var.name_prefix
+  node_service_account_email = module.gke.service_account_email
+  internal                   = var.internal_load_balancer
+  ingress_cidr_blocks        = var.ingress_cidr_blocks
+  instance_name              = local.materialize_instance_name
+  namespace                  = local.materialize_instance_namespace
+  resource_id                = module.materialize_instance.instance_resource_id
+
+  depends_on = [
+    module.materialize_instance,
+  ]
+}
+
+# -----------------------------------------------------------------------------
+# Ory: Identity & OAuth2 (Kratos + Hydra)
+# -----------------------------------------------------------------------------
+
+module "ory_kratos" {
+  source = "../../../kubernetes/modules/ory-kratos"
+
+  namespace = "ory"
+  dsn       = local.ory_kratos_dsn
+
+  node_selector = local.generic_node_labels
+
+  # Kratos requires at least one identity schema and a default browser return URL
+  identity_schemas = {
+    "identity.default.schema.json" = jsonencode({
+      "$id"     = "https://schemas.ory.sh/presets/kratos/identity.basic.schema.json"
+      "$schema" = "http://json-schema.org/draft-07/schema#"
+      title     = "Default Identity Schema"
+      type      = "object"
+      properties = {
+        traits = {
+          type = "object"
+          properties = {
+            email = {
+              type   = "string"
+              format = "email"
+              title  = "Email"
+              "ory.sh/kratos" = {
+                credentials = {
+                  password = { identifier = true }
+                }
+                recovery     = { via = "email" }
+                verification = { via = "email" }
+              }
+            }
+          }
+          required = ["email"]
+        }
+      }
+    })
+  }
+
+  helm_values = {
+    kratos = {
+      config = {
+        selfservice = {
+          default_browser_return_url = var.ory_issuer_url
+        }
+        identity = {
+          default_schema_id = "default"
+          schemas = [
+            {
+              id  = "default"
+              url = "file:///etc/config/identity.default.schema.json"
+            }
+          ]
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    module.gke,
+    module.ory_database,
+    module.generic_nodepool,
+    module.coredns,
+  ]
+}
+
+module "ory_hydra" {
+  source = "../../../kubernetes/modules/ory-hydra"
+
+  namespace = "ory"
+  # Share the namespace already created by Kratos
+  create_namespace = false
+
+  dsn        = local.ory_hydra_dsn
+  issuer_url = var.ory_issuer_url
+
+  # Point Hydra login/consent flows to Kratos
+  login_url   = "${module.ory_kratos.public_url}/self-service/login/browser"
+  consent_url = "${module.ory_kratos.public_url}/self-service/login/browser"
+  logout_url  = "${module.ory_kratos.public_url}/self-service/logout/browser"
+
+  node_selector = local.generic_node_labels
+
+  depends_on = [
+    module.gke,
+    module.ory_database,
+    module.ory_kratos,
+    module.generic_nodepool,
+    module.coredns,
+  ]
+}
