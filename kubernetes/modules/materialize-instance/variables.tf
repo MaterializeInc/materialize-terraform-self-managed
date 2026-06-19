e6b230e4d40157fe7b6b1dbea92289b9b6d611df
Materialize <infra+bot@materialize.com>
2026-06-19 14:07:43 +0000

    Bump to helm-chart v26.29.0

--- kubernetes/modules/materialize-instance/variables.tf
+++ kubernetes/modules/materialize-instance/variables.tf
@@ -53,7 +53,7 @@ variable "license_key" {
 variable "environmentd_version" {
   description = "Version of environmentd to use"
   type        = string
-  default     = "v26.28.0" # META: mz version
+  default     = "v26.29.0" # META: mz version
   nullable    = false
 }
 
