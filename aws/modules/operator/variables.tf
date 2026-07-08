a862cea91dea68b3908da928b0d55d12adaa7da4
Dennis Felsing <dennis@felsing.org>
2026-07-08 13:07:32 +0200

    Bump to helm-chart v26.31.2 (#271)

--- aws/modules/operator/variables.tf
+++ aws/modules/operator/variables.tf
@@ -7,7 +7,7 @@ variable "name_prefix" {
 variable "operator_version" {
   description = "Version of the Materialize operator to install"
   type        = string
-  default     = "v26.31.1" # META: helm-chart version
+  default     = "v26.31.2" # META: helm-chart version
   nullable    = false
 }
 
